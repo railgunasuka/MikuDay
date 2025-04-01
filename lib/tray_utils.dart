1bda4f4d73d74aeb51ec2930b81951515a9df7a3
railgunasuka <806154939@qq.com>
2025-04-01 17:35:01 +0800

    version13

--- lib/tray_utils.dart
+++ lib/tray_utils.dart
@@ -2,38 +2,16 @@
 import 'package:tray_manager/tray_manager.dart';
 import 'package:window_manager/window_manager.dart';
 
-
 Future<void> initSystemTray() async {
   // 设置系统托盘图标
   await trayManager.setIcon("assets/cong.ico");
-  // 新建菜单
-  List<MenuItem> items = [
 
-
-    MenuItem(
-      key: 'hide',
-      label: '隐藏',
-    ),
-    
-    
-
-
-    MenuItem.checkbox(
-      key: 'toggle_top',
-      label: '置顶',
-      
-      checked: await windowManager.isAlwaysOnTop(),
-      onClick: (MenuItem menuItem) {menuItem.checked = !(menuItem.checked == true);},
-    ),
-
-    MenuItem(
-      key: 'quit',
-      label: '退出',
-    ),
-
-
-
+  // 初始化菜单（初始状态由 TrayWatcher 管理）
+  final items = [
+    MenuItem(key: 'hide', label: '隐藏'),
+    MenuItem.checkbox(key: 'toggle_top', label: '置顶', checked: false),
+    MenuItem(key: 'quit', label: '退出'),
   ];
-  // 设置菜单
+
   await trayManager.setContextMenu(Menu(items: items));
 }
--- lib/tray_wather.dart
+++ lib/tray_wather.dart
@@ -13,10 +13,25 @@ class TrayWatcher extends StatefulWidget {
 }
 
 class _TrayWatcherState extends State<TrayWatcher> with TrayListener {
+  static Future<void> updateTopMenuItem(bool isTop) async {
+    final items = [
+      MenuItem(key: 'hide', label: '隐藏'),
+      MenuItem.checkbox(key: 'toggle_top', label: '置顶', checked: isTop),
+      MenuItem(key: 'quit', label: '退出'),
+    ];
+    await trayManager.setContextMenu(Menu(items: items));
+  }
+
   @override
   void initState() {
     super.initState();
     trayManager.addListener(this);
+    _initTopState();
+  }
+
+  Future<void> _initTopState() async {
+    final isTop = await windowManager.isAlwaysOnTop();
+    await updateTopMenuItem(isTop);
   }
 
   @override
@@ -33,7 +48,7 @@ class _TrayWatcherState extends State<TrayWatcher> with TrayListener {
   @override
   void onTrayIconRightMouseDown() async {
     // 右键点击，打开菜单
-    await trayManager.popUpContextMenu(bringAppToFront:true);
+    await trayManager.popUpContextMenu(bringAppToFront: true);
   }
 
   @override
@@ -41,28 +56,22 @@ class _TrayWatcherState extends State<TrayWatcher> with TrayListener {
     windowManager.show(); // 该方法来自window_manager插件
   }
 
-
-
   @override
   Future<void> onTrayMenuItemClick(MenuItem menuItem) async {
     switch (menuItem.key) {
-
-
       case 'hide':
         windowManager.hide();
         break;
 
-
-
-      
       case 'toggle_top':
         bool isAlwaysOnTop = await windowManager.isAlwaysOnTop();
         await windowManager.setAlwaysOnTop(!isAlwaysOnTop);
+        await updateTopMenuItem(!isAlwaysOnTop);
         break;
-     
+
       case 'quit':
         exit(0);
-      
+
       default:
         break;
     }
